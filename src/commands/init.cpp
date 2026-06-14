77a2c5405b26fdd1f2f63bbe4ce001fccf91a622
Prathmesh Urkude <prathmeshurkude20@gmail.com>
2026-06-14 21:51:39 +0530

    Initialize VCX repository structure (.vcx file)

--- src/commands/init.cpp
+++ src/commands/init.cpp
@@ -1,7 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 #include "init.h"
+#include<filesystem>
+#include <fstream>
+using namespace std::filesystem;
 
 void initCommand(){
+    if(exists(".vcx")){
+        cout << "vcx repository already exists in this directory." << endl;
+        cout << "do you want to reinitialize the repository, (y/n)? : ";
+
+        char choice;
+        cin >> choice;
+        choice = tolower(choice);
+        if('y' == choice) {
+            remove_all(".vcx");
+            cout << "Removed existing vcx repository." << endl;
+        } else return;
+    }
+
+    create_directory(".vcx");
+    create_directory(".vcx/objects");
+    create_directories(".vcx/refs/heads/");
+    create_directories(".vcx/refs/tags/");
+
+    ofstream head(".vcx/HEAD");
+    head << "ref: refs/heads/main\n";
+    head.close();
+
+    ofstream main_branch(".vcx/refs/heads/main");
+    main_branch.close();
+
+    ofstream indexFile(".vcx/index", ios::binary);
+    indexFile.close();
+
+
     cout << "Initialized empty vcx repository in ./.vcx/" << endl;
 }
